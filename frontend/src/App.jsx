06cc1e4224d328fef9014c8e4175304ea2a302a2
Reda Bourial <reda.bourial@gmail.com>
2024-03-21 03:18:39 +0100

    added form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Home} from "./pages/home"
-import { Profile } from "./pages/profile";
+import { Home, Profile } from "./pages"
 
 function App() {
   return (
--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,7 +1,53 @@
 import React from "react";
 
+import { MailOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input } from 'antd';
+
+import './styles.css';
+
 export function Home() {
-    return <>
-        Home
-    </>
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        
+    };
+
+    return (
+        <div className="container" >
+            <Form
+                className="form"
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Username!',
+                        },
+                    ]}
+                >
+                    <Input prefix={<UserOutlined />} placeholder="Username" />
+                </Form.Item>
+                <Form.Item
+                    name="email"
+                    rules={[
+                        {
+                            required: true,
+                            type: "email",
+                            message: "The input is not valid E-mail!",
+                        },
+                    ]}
+                >
+                    <Input prefix={<MailOutlined />} placeholder="Email" />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className="register-form-button">
+                        Register
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
+
 }
