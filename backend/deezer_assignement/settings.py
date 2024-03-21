5533d4f6f48dc8ed01d0b983ab4819fd44f8cd82
Reda Bourial <reda.bourial@gmail.com>
2024-03-21 16:09:40 +0100

    fixed serving static files from django

--- backend/deezer_assignement/settings.py
+++ backend/deezer_assignement/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
     'users',
@@ -121,8 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/assets/'
+STATIC_ROOT = "./static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- backend/deezer_assignement/urls.py
+++ backend/deezer_assignement/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path,include
+from django.urls import path,re_path,include
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
-    path('api/users/', include('users.urls')),
+    path('api/', include('users.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += [re_path(r'^.*/$', serve, {'document_root': settings.STATIC_ROOT, 'path': 'index.html'})]
