195acdaca8d6b4be479a83e0b1bd971b1aa8cb3d
Reda Bourial <reda.bourial@gmail.com>
2024-03-23 02:46:44 +0100

    removed bloat

--- backend/deezer_assignement/settings.py
+++ backend/deezer_assignement/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "static/"
-STATICFILES_DIRS = [STATIC_ROOT]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
