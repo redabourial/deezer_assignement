64d69e5dfbd80c6a41f2c28666731be4af08f488
Reda Bourial <reda.bourial@gmail.com>
2024-03-24 20:05:24 +0100

    fixed favicon serving in production

--- backend/deezer_assignement/urls.py
+++ backend/deezer_assignement/urls.py
@@ -1,9 +1,15 @@
 from django.conf import settings
-from django.urls import include, re_path
+from django.urls import include, re_path, path
 from django.views.static import serve
 
 urlpatterns = [
     re_path(r"^api/", include("users.urls")),
+    # Serve favicon
+    path(
+        "favicon.ico",
+        serve,
+        {"document_root": settings.STATIC_INDEX_ROOT, "path": "favicon.ico"},
+    ),
     # Catch-all for serving index.html
     re_path(
         r"^.*$",
