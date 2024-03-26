21dc1704b2d904899f933ea89b796acd83f28e12
Reda Bourial <reda.bourial@gmail.com>
2024-03-26 02:12:43 +0100

    removed unused imports

--- backend/users/tests/test_fibonacci.py
+++ backend/users/tests/test_fibonacci.py
@@ -1,7 +1,4 @@
 from django.test import TestCase
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APIClient
 
 from users.models.user import fibonacci
 
