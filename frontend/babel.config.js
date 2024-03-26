3fa56e568473be737d990cf889d77e90e011f53b
Reda Bourial <reda.bourial@gmail.com>
2024-03-26 01:05:46 +0100

    fixed babel warning

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-    presets: [
-      '@babel/preset-env',
-      '@babel/preset-react',
-    ],
-  };
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ],
+  env: {
+    development: {
+      compact: false,
+    },
+    production: {
+      compact: true,
+    },
+  },
+};
